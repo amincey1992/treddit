9c8facaa0d6b4c4eee06f2f5f4d601a6ab2e51dd
alex <amincey1992@gmail.com>
2016-04-21 10:47:13 -0400

    merge

--- db/schema.rb
+++ db/schema.rb
@@ -11,13 +11,17 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160421134332) do
+ActiveRecord::Schema.define(version: 20160421141057) do
 
   create_table "posts", force: :cascade do |t|
     t.string   "author"
     t.text     "post_entry"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
+    t.datetime "created_at",          null: false
+    t.datetime "updated_at",          null: false
+    t.string   "avatar_file_name"
+    t.string   "avatar_content_type"
+    t.integer  "avatar_file_size"
+    t.datetime "avatar_updated_at"
   end
 
 end
