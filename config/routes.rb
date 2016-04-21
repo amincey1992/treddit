bee93b68dedcb78b911ea66b3f7a0ad577c5400d
Kenan <kenanbali@hotmail.com>
2016-04-21 09:47:17 -0400

    added scaffold and routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,8 @@
 Rails.application.routes.draw do
-  get 'treddit/index'
+  resources :posts
+  root 'treddit#index'
 
-  get 'treddit/post'
+  get 'post' => 'treddit#post'
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
